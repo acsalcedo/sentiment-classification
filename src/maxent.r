c98e51839f6b8feff99412a1312724cddd57b2ad
Andrea Salcedo <andreacsalcedo@gmail.com>
2016-03-26 00:08:08 -0430

    Agregado método de maxent.

---
+++ src/maxent.r
@@ -0,0 +1,78 @@
+library(maxent)
+
+# Reads data from file where all reviews are stored.
+data <- read.csv(file="../data/divided/all.csv")
+
+# Sorts data in random order so not all 
+# positive and negative reviews are together.
+sortedData <-  data[sample(nrow(data)),]
+dataSize <- nrow(sortedData)
+
+corpus <- Corpus(VectorSource(sortedData$review[1:dataSize]))
+matrix <- DocumentTermMatrix(corpus)
+
+sparse <- as.compressed.matrix(matrix)
+
+# Training set which contains 70% of the data.
+trainIndex <- ceiling(0.7*dataSize)
+trainSet <- sparse[1:trainIndex,]
+trainSetClass <- sortedData$class[1:trainIndex]
+
+# Test set which has the leftover 30%.
+testSet <- sparse[trainIndex:dataSize,]
+
+# Calculates max entropy model.
+model <- maxent(trainSet,trainSetClass)
+
+# Classifies test set given the max entropy model.
+classifedTestSet <- predict(model,testSet)
+
+getConfusionMatrix <- function(results) {
+
+    posAsPos <- 0
+    posAsNeg <- 0
+    negAsNeg <- 0
+    negAsPos <- 0
+    i <- 1
+    resultSize <- nrow(results)
+    
+    while (i <= resultSize) {
+        
+        if (results[[i,1]] == "positive") {
+            if (results[i,2] > 0.5) {
+                posAsPos <- posAsPos + 1
+            } else {
+                posAsNeg <- posAsNeg + 1
+            }
+        } else {
+            if (results[[i,3]] > 0.5) {
+                negAsNeg <- negAsNeg + 1
+            } else {
+                negAsPos <- negAsPos + 1
+            }    
+        }
+        i <- i + 1
+    }
+    result <- matrix(c(posAsPos,posAsNeg,negAsPos,negAsNeg),ncol=2,byrow=TRUE)
+    colnames(result) <- c("positive","negative")
+    rownames(result) <- c("positive","negative")
+    result <- as.table(result)
+    return (result)
+}
+
+confusionMatrix <- getConfusionMatrix(classifedTestSet)
+truePositive <- confusionMatrix[1]
+falsePositive <- confusionMatrix[2]
+trueNegative <- confusionMatrix[4]
+falseNegative <- confusionMatrix[3]
+
+accuracy <- (sum(diag(confusionMatrix)) / nrow(testSet)) * 100
+precision <- (truePositive / (truePositive + falsePositive))
+sensitivity <- (truePositive / (truePositive + falseNegative))
+
+cat("\nConfusion Matrix\n")
+print(confusionMatrix)
+cat("\n")
+cat("Accuracy:", accuracy, "\n")
+cat("Precision:", precision, "\n")
+cat("Recall:", sensitivity, "\n")
--- src/review.py
+++ src/review.py
@@ -41,7 +41,17 @@ def getRecentReviews():
     with open(dataFolder+folder+fileName+timestr+extension, "w") as reviewsFile:
         reviewsFile.write(response.content)
 
+def getReview(id):
 
+    extension = ".xml"
+    folder = "individual/"
+
+    payload = {'id': id, 'key': key}
+    response = requests.get("https://www.goodreads.com/review/show.xml?", params=payload,stream=True)
+
+    with open(dataFolder+folder+str(id)+extension, "w") as reviewsFile:
+        reviewsFile.write(response.content)
+    
 def getReviewsList():
 
     reviewsList = []
@@ -71,6 +81,30 @@ def getReviewsList():
                 reviewsList.append(review)
                 IDdict.append(id)
 
+    folder = "individual/"
+
+    for fileName in os.listdir(dataFolder+folder):
+
+        f = open(os.path.join(dataFolder+folder, fileName), "r")
+        
+        # print fileName
+        xml = f.read()
+        root = ET.fromstring(xml)
+
+        for r in root.iter('review'):
+
+            id = r.find('id').text
+            rating = int(r.find('rating').text)
+            body = r.find('body').text
+            
+            review = Review(id,rating,body.encode('utf-8'))
+
+            # if (rating == 3):
+            #     print review 
+            
+            if not id in IDdict:
+                reviewsList.append(review)
+                IDdict.append(id)
 
     return reviewsList
 
@@ -160,4 +194,38 @@ reviewsList = getReviewsList()
 printReviews2(reviewsList)
 printReviews(reviewsList)
 
+# 377358643,1548502321,746865698,
+# 1569055692,1577315128,1566245477,
+# 1561843533,1451748576,1542175171,
+# 1583310336,1582974384,1360072687,
+# 1063773072,1059502467,1059483839,
+# 21231663,1251967947,1189419307,
+# 1183458732,941508925
 
+# idList = [34309215,7478942,5589709,
+#         5546013,158864113,154964194,
+#         1555369413,1532353051,1349125869,
+#         1282950057,1272048203,1237177100,
+#         1166760739,1166755502,1166746044,
+#         1249888450,1179687088,1167379055,
+#         1166730306,1112477094,1112477073,
+#         1523827452,1468886860,1516634386,
+#         1495482218,1176243792,1127079191,
+#         491009615,513898435,67396109,
+#         52797224,40925132,38067634,
+#         18037183,15702592,10834229,
+#         7601879,6442362,6184434,
+#         6101314,6069952,723058380,
+#         42000372,40375436,37196743,
+#         21622303,9627988,8218815,
+# #         42614989]
+
+
+# idList = [1583101020,1416674989,1560541347,
+#         1278970311,850881820,35272288,
+#         737606973,363645711,188642096,
+#         64059313,1397645608,22779567,
+#         115912781,281408391,281408391,
+#         657546617,549733504]
+# for id in idList:
+#     getReview(id)
